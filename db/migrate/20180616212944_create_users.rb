bbd767e8934e92daedda9232ff0a5ab2f8e0f922
federicomoyamartin <federicomoyamartin@gmail.com>
2018-06-16 18:43:08 -0300

    Create users

--- db/migrate/20180616212944_create_users.rb
+++ db/migrate/20180616212944_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string :ip
+      t.string :user
+      t.string :email
+      t.boolean :status
       t.timestamps
     end
   end
