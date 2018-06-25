273e89b478dd8e882931683d6adb85641dd9ace9
federicomoyamartin <federicomoyamartin@gmail.com>
2018-06-24 21:47:50 -0300

    Final changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    @users = User.where.not(["id = ?", params[:id]])
     if user_params[:status].present?
       @users = @users.where(status:user_params[:status])
     end
@@ -53,15 +53,20 @@ class UsersController < ApplicationController
   end
 
   def tweet
-    @user = User.where(user:user_params[:user]).first
-    @user.tweet(user_params[:tweet])
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "WN2FQ7v8bEsZMGx7pYWJ0Bhc5"
+      config.consumer_secret     = "T7uRyp8C7sFolKWxXjQNq6XwTR0rnVmPaifN0QW1a22HNsosm4"
+      config.access_token        = "1008136843030859782-VE1Gd2QYUygaDRezL77hqSQqR6zhqS"
+      config.access_token_secret = "j6pI1Fzw9US1NCnREqd794Bn9qh4gl48V7tbsnSbL0bWF"
+    end
+    client.update(user_params[:message])
     json_response("Todo bien")
   end
 
   private
 
   def user_params
-    params.permit(:ip, :user, :email, :status, :password, :created_by, :updated_at,:tweet)
+    params.permit(:ip, :user, :email, :status, :password, :created_by, :updated_at,:message)
   end
 
   def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,13 +19,13 @@ class User < ApplicationRecord
   validates :ip, uniqueness: true, presence: true
 
 
-  def tweet(message)
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "WN2FQ7v8bEsZMGx7pYWJ0Bhc5"
-      config.consumer_secret     = "T7uRyp8C7sFolKWxXjQNq6XwTR0rnVmPaifN0QW1a22HNsosm4"
-      config.access_token        = "1008136843030859782-VE1Gd2QYUygaDRezL77hqSQqR6zhqS"
-      config.access_token_secret = "j6pI1Fzw9US1NCnREqd794Bn9qh4gl48V7tbsnSbL0bWF"
-    end
-    client.update(message)
-  end
+  # def tweet(message)
+  #   client = Twitter::REST::Client.new do |config|
+  #     config.consumer_key        = "WN2FQ7v8bEsZMGx7pYWJ0Bhc5"
+  #     config.consumer_secret     = "T7uRyp8C7sFolKWxXjQNq6XwTR0rnVmPaifN0QW1a22HNsosm4"
+  #     config.access_token        = "1008136843030859782-VE1Gd2QYUygaDRezL77hqSQqR6zhqS"
+  #     config.access_token_secret = "j6pI1Fzw9US1NCnREqd794Bn9qh4gl48V7tbsnSbL0bWF"
+  #   end
+  #   client.update(message)
+  # end
 end
